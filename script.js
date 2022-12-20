9905403769238dc6e41dd4c029c16e3da6f552fa
dm1triiPonomarev <119684791+dm1triiPonomarev@users.noreply.github.com>
2022-12-20 20:20:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 'use strict';
+
 let name = document.querySelector('.name')
 let num = document.querySelector('.number-js')
-let plusBtn = document.querySelector('.bnt-js-plus')
+let plusBtn = document.querySelector('.bnt-js-plus');
 let minusBtn = document.querySelector('.bnt-js-minus');
+let firstName = document.querySelector('.first_name');
+let secondName = document.querySelector('.second_name');
+
+
+
+let Monday =['Жим гантелей лежа','жим лежа на наклонной скамье ','отжимание от брусьев','французский жим штанги лежа'];
+let Wednesday = ['Подтягивание на турнике','тяга горизонтального блока к поясу сидя','тяги гантели одной рукой в наклоне','сгибание рук со штангой стоя']
+
+let Friday =['жим ногами','разгибание ног','сгибание ног','армейский жим штанги стоя','подъем гантелей через стороный']
 
 let counter = 0;
 let nameDelete = 0;
 let mass = ['Пресс','Плечи','Ноги',]
 let del =['all']
-name.textContent = `${mass[nameDelete]} (${nameDelete + 1} / ${mass.length })`;
+
+let day = new Date();
+
+
+
 
 function changeBackground(color) {
 	 plusBtn.style.background = color;
@@ -51,10 +65,7 @@ plusBtn.addEventListener('click',function(){
 
 
 minusBtn.addEventListener('click',function(){
-	nameDelete += 1 ;
-	if(nameDelete == mass.length ){
-		nameDelete = 0
-	}
+
 
 	//counter
 	counter = 0
@@ -65,7 +76,7 @@ minusBtn.addEventListener('click',function(){
 	setTimeout(changeBackground2('firebrick'),3000)
 
 	//
-	name.textContent = `${mass[nameDelete]} (${nameDelete + 1} / ${mass.length })`;
+	
 	// if(name.textContent ==  null){
 	// 	name.textContent = del[0]
 	// }else{}
@@ -74,4 +85,36 @@ minusBtn.addEventListener('click',function(){
 
 })
 
+function nameToDate(arr){
+	firstName.textContent = `${arr[nameDelete]}`
+	secondName.textContent = `(${nameDelete + 1} / ${arr.length })`
+	// name.textContent = `${arr[nameDelete]} (${nameDelete + 1} / ${arr.length })`;
 
+	minusBtn.addEventListener('click',function(){
+		nameDelete += 1 ;
+		if(nameDelete == arr.length ){
+			nameDelete = 0
+		}
+		firstName.textContent = `${arr[nameDelete]} `
+		secondName.textContent = `(${nameDelete + 1} / ${arr.length })`
+		// name.textContent = `${arr[nameDelete]} 
+		//  (${nameDelete + 1} / ${arr.length })`;
+	})
+}
+
+if(day.getDay() == 1){
+	nameToDate(Monday)
+}
+
+if(day.getDay() == 3){
+	nameToDate(Wednesday)
+}
+
+
+if(day.getDay() == 5){
+	nameToDate(Friday)
+}
+
+if(day.getDay() == 0,2,4,6){
+	nameToDate(Wednesday)
+}
